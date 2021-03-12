35152a44c2130021d0b444fdea273557b12d5931
秃尾巴的猫 <472647301@qq.com>
2021-03-12 17:12:46 +0800

    update loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,36 +1,44 @@
 import React, { useState } from "react";
 import { Image } from "@nodegui/react-nodegui";
 import { AspectRatioMode } from "@nodegui/nodegui";
-import path from "path";
+import l1 from "../images/l1.jpeg";
+import l2 from "../images/l2.jpeg";
+import l3 from "../images/l3.jpeg";
+import l4 from "../images/l4.jpeg";
+import l5 from "../images/l5.jpeg";
+import l6 from "../images/l6.jpeg";
+import l7 from "../images/l7.jpeg";
+import l8 from "../images/l8.jpeg";
 
-const rootDir = path.resolve(__dirname, "../..");
-const assetUrl = path.resolve(rootDir, "src/images");
+export const icons = [l1, l2, l3, l4, l5, l6, l7, l8];
 
 type IconProps = {
-  icon: string;
+  index: number;
 };
 export const Icon = (props: IconProps) => {
-  const iconId = props.icon || "l1";
-  const url = path.resolve(assetUrl, `${iconId}.jpeg`);
-  return <Image src={url} aspectRatioMode={AspectRatioMode.KeepAspectRatio} />;
+  const i = props.index - 1;
+  return (
+    <Image src={icons[i]} aspectRatioMode={AspectRatioMode.KeepAspectRatio} />
+  );
 };
 
 export const Loading = () => {
-  let timer: NodeJS.Timeout | null = null;
   const [index, setIndex] = useState(1);
+  let timer: NodeJS.Timeout | null = null;
+  const interval = () => {
+    const now = index + 1;
+    if (now > 8) {
+      setIndex(1);
+    } else {
+      setIndex(now);
+    }
+  };
   React.useEffect(() => {
-    timer = setInterval(() => {
-      const now = index + 1;
-      if (now > 8) {
-        setIndex(1);
-      } else {
-        setIndex(now);
-      }
-    }, 300);
+    timer = setInterval(interval, 300);
     return () => {
       timer && clearInterval(timer);
       timer = null;
     };
-  }, []);
-  return <Icon icon={`l${index}`} />;
+  }, [index]);
+  return <Icon index={index} />;
 };
